8b89141d7c94a7b55e320b4853b8a4520ad8dd38
MD Atick <115481550+MD-ATICK@users.noreply.github.com>
2024-06-19 06:57:32 -0600

    meta tag add

--- index.js
+++ index.js
@@ -9,25 +9,23 @@ const checkerRouter = require("./router/CheckerRoute");
 const { databaseConnect } = require("./utils/DatabaseConnect");
 const moment = require("moment");
 const emailvalidator = require("deep-email-validator");
+const path = require('path')
 
 app.use(
   cors({
-    origin:
-      ["https://6672b66dedf85b0497ad5cfb--spiffy-salamander-bf0160.netlify.app"  , 'http://localhost:5173'],
+    origin: [
+      "https://6672b66dedf85b0497ad5cfb--spiffy-salamander-bf0160.netlify.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
   })
 );
 app.use(express.json());
 databaseConnect();
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", async (req, res) => {
-  const {
-    data: { ip },
-  } = await axios.get("https://jsonip.com/");
-  res.send({
-    say: "Hello world!",
-    ip,
-  });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.use("/api/v1/gmail", checkerRouter);
