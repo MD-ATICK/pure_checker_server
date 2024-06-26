2ccc7fcd55ac83e71343520b600a316dead1e62e
MD Atick <115481550+MD-ATICK@users.noreply.github.com>
2024-06-26 01:24:00 -0600

    shsd

--- controllers/checker.js
+++ controllers/checker.js
@@ -39,52 +39,54 @@ class checker {
 
       const data = await validate(email);
 
-      if(data){
-        if (token === "null" || !token) {
-          console.log("1");
-          const x = await axios.get("https://jsonip.com");
-          const ip = x.data.ip;
-          const userIp = await UserIp.findOneAndUpdate(
-            { ip },
-            { $inc: { freeCredit: -1 } },
-            { new: true }
-          );
-          if (!userIp) return resReturn(res, 222, { err: "ip not found." });
-          resReturn(res, 200, {
-            data: { ...data, email },
-            userIp,
-            userStatus: "default",
-          });
+      // if(data){
+      //   if (token === "null" || !token) {
+      //     console.log("1");
+      //     const x = await axios.get("https://jsonip.com");
+      //     const ip = x.data.ip;
+      //     const userIp = await UserIp.findOneAndUpdate(
+      //       { ip },
+      //       { $inc: { freeCredit: -1 } },
+      //       { new: true }
+      //     );
+      //     if (!userIp) return resReturn(res, 222, { err: "ip not found." });
+      //     resReturn(res, 200, {
+      //       data: { ...data, email },
+      //       userIp,
+      //       userStatus: "default",
+      //     });
   
-        } else if (token !== "null") {
-          console.log("3");
-          const smtp = data.validators.smtp.valid;
-          await jwt.verify(
-            token,
-            process.env.jwt_secret,
-            async (err, verifiedJwt) => {
-              if (err) return resReturn(res, 223, { err: err.message });
-              const find = await User.findByIdAndUpdate(
-                verifiedJwt._id,
-                {
-                  $inc: {
-                    credit: -1,
-                    invalid: smtp === false && 1,
-                    deliverable: smtp === true && 1,
-                  },
-                },
-                { new: true }
-              );
+      //   } else if (token !== "null") {
+      //     console.log("3");
+      //     const smtp = data.validators.smtp.valid;
+      //     await jwt.verify(
+      //       token,
+      //       process.env.jwt_secret,
+      //       async (err, verifiedJwt) => {
+      //         if (err) return resReturn(res, 223, { err: err.message });
+      //         const find = await User.findByIdAndUpdate(
+      //           verifiedJwt._id,
+      //           {
+      //             $inc: {
+      //               credit: -1,
+      //               invalid: smtp === false && 1,
+      //               deliverable: smtp === true && 1,
+      //             },
+      //           },
+      //           { new: true }
+      //         );
   
-              console.log({ find });
-              resReturn(res, 200, {
-                data: { ...data, email },
-                user: find,
-                userStatus: "login",
-              });
-            }
-          );
-        }
+      //         console.log({ find });
+      //         resReturn(res, 200, {
+      //           data: { ...data, email },
+      //           user: find,
+      //           userStatus: "login",
+      //         });
+      //       }
+      //     );
+      //   }
+
+      res.send(data)
       }
 
     } catch (error) {
