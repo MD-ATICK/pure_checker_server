ce1d1556a9fc07747cc2e6aec0f869564d27c3e6
MD Atick <115481550+MD-ATICK@users.noreply.github.com>
2024-06-26 08:49:26 -0600

    slfjslfj

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/a", (req, res) => {
+app.get("/email", (req, res) => {
   validate("mdatick866@gmail.com")
     .then((data) => {
       res.json(data);
