8e6018e7eb3457eebc0f223b064a9ec5aac1241a
MD Atick <115481550+MD-ATICK@users.noreply.github.com>
2024-06-26 22:52:15 -0600

    fdldfjlsj

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const { databaseConnect } = require("./utils/DatabaseConnect");
 const moment = require("moment");
 const path = require("path");
 const { validate } = require("deep-email-validator");
+// const dns = require("dns");
+// const net = require("net");
+// const { Buffer } = require("buffer");
 
 app.use(
   cors({
@@ -23,21 +26,30 @@ app.use(express.json());
 databaseConnect();
 app.use(express.static(path.join(__dirname, "public")));
 
+async function isValidEmailDomain(email) {
+  const domain = email.split("@")[1];
+  try {
+    const addresses = await dns.promises.resolve(domain, "MX");
+    return addresses.length > 0;
+  } catch (err) {
+    return false; // DNS lookup failed
+  }
+}
+
 app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/email", (req, res) => {
-  validate("mdatick866@gmail.com")
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((error) => {
-      console.error("Validation error is:", error);
-      res
-        .status(500)
-        .json({ error: "Email validation failed", details: error.message });
-    });
+app.get("/email", async (req, res) => {
+  try {
+    const result = await validate("mdatick866@gmail.com");
+    res.status(200).send({ result });
+  } catch (error) {
+    console.error("Validation error is:", error);
+    res
+      .status(500)
+      .json({ error: "Email validation failed", details: error.message });
+  }
 });
 
 app.use("/api/v1/gmail", checkerRouter);
