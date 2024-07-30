00209bdc1b20df753e3583a279809f16c002ddec
MD Atick <115481550+MD-ATICK@users.noreply.github.com>
2024-07-30 17:43:51 +0600

    fdljfd

--- router/MaintenanceRoute.js
+++ router/MaintenanceRoute.js
@@ -1,10 +1,10 @@
-// const express = require("express");
-// const maintenance = require("../controllers/maintenance");
-// const router = express.Router();
+const express = require("express");
+const maintenance = require("../controllers/maintenance.js");
+const router = express.Router();
 
-// router.get("/create", maintenance.create);
-// router.get("/remove", maintenance.remove);
-// router.get("/checking", maintenance.checking);
-// router.get("/all-maintenance", maintenance.getAll);
+router.get("/create", maintenance.create);
+router.get("/remove", maintenance.remove);
+router.get("/checking", maintenance.checking);
+router.get("/all-maintenance", maintenance.getAll);
 
-// module.exports = router;
+module.exports = router;
--- router/userRoute.js
+++ router/userRoute.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const user = require("../controllers/user");
 const { userAuthorize } = require("../utils/utils");
-// const Volume = require("../controllers/Volume");
 const router = express.Router();
 
-// user
 router.get("/auth", userAuthorize, user.auth);
 router.post("/register", user.register); // email auth
 router.post("/login", user.login); // email auth
@@ -12,43 +10,5 @@ router.post("/googleLogin", user.googleLogin);
 router.get("/ip-free-credit", user.authByIp);
 router.put("/update-password", userAuthorize, user.updatePassword); // todo : edited pass to password
 router.put("/update-profile", userAuthorize, user.updateProfile);
-
-// done. admin edit.
-// router.delete("/delete", user.deleteMany); d
-// router.get("/getUsers", userAuthorize, user.getMany);
-// router.get("/block/:_id", user.block);
-// router.post("/add-user", user.addUser); // email auth
-// router.post("/adjust/:_id", user.adjustUser);
-
-// done; user 2 step authentication done ;
-// router.get("/otpSent/:email", user.sendOTP);
-// router.post("/verifyOTP", user.verifyOTP);
-
-// done : volume
-// router.get("/get-volume", Volume.get);
-// router.post("/create-volume", userAuthorize, Volume.add);
-// router.put("/update-volume", userAuthorize, Volume.update);
-// router.delete("/delete-volume/:_id", userAuthorize, Volume.delete);
-
-// done : plan
-// router.post("/buy-plan", userAuthorize, user.buyPlan);
-// router.get("/subscription", userAuthorize, user.subscription);
-
-// done : apis
-// in api route. named "/apis" @@@@ done
-// router.get("/get-api", userAuthorize, user.getApis);
-// router.post("/create-api", userAuthorize, user.createApi);
-// router.get("/total-mail-check", user.totalMailCheck);
-
-// done: payment ### done
-// router.get("/payments", userAuthorize, user.getPayments);
-// router.get("/all-payments", user.getAllPayments); // add
-
-// done: mail sent ## done
-// router.post("/mail-sent", user.mailSent);
-// router.post("/contactUs", user.contactUSMail);
-// router.post("/verify-mail-sent/", user.verifyMailSent);
-// router.post("/forget-password/:token", user.forgetPassword);
-// router.get("/emailAuthCheck/:token", user.emailAuthCheck);
 
 module.exports = router;
